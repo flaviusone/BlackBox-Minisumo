ce7eff90d106980b85341cb45895436d2a2fbc1f
flaviusone <flaviusone@yahoo.com>
2013-04-23 21:16:46 +0300

    Incearcare fara delay cu millis

--- atac.ino
+++ atac.ino
@@ -8,7 +8,8 @@ void atac()
     /* ifuri sharpuri */
     if ((SL>pragsharp && SR>pragsharp)  || (digitalRead(BF) == 0) ) //inainte
     {  
-      //x=3;
+      x=1;
+      xprec=10;
       break;
     }
     else if (SL>pragsharp && SR<pragsharp) //rotire stanga
@@ -29,38 +30,48 @@ void atac()
 
     /* daca a fost activat BL*/
     if(xprec == 1){
-      if(millis() - previousMillis1 > delay_BL)
-      {
-        x = 1;      /* am depasit deci return in default*/
-        xprec = 10; /* o valoare random*/
-        previousMillis1=millis();
-      }
-      else x = 4; /*RotireStangaRapid*/
+      x = 4;
     }
-
-
-    /* daca a fost activat BR*/
     if(xprec == 2){
-      if(millis() - previousMillis2 > delay_BR)
-      {
-        x = 1;       /* am depasit deci return in default*/
-        xprec = 10; /* o valoare random*/
-        previousMillis2=millis();
-      }
-      else x = 5 ;/*RotireDreaptaRapid*/
+      x = 5;
     }
-
-
-    /* daca a fost activat BB*/
     if(xprec == 3){
-      if(millis() - previousMillis3 > delay_BB)
-      {
-        x = 1;      /* am depasit deci return in default*/
-        xprec = 10; /* o valoare random*/
-        previousMillis3=millis();
-      }
-      else x = 5 ;/*RotireDreaptaRapid*/
+      x = 5;
     }
+    
+    // if(xprec == 1){
+    //   if(millis() - previousMillis1 > delay_BL)
+    //   {
+    //     x = 1;      /* am depasit deci return in default*/
+    //     xprec = 10; /* o valoare random*/
+    //     previousMillis1=millis();
+    //   }
+    //   else x = 4; /*RotireStangaRapid*/
+    // }
+
+
+    // /* daca a fost activat BR*/
+    // if(xprec == 2){
+    //   if(millis() - previousMillis2 > delay_BR)
+    //   {
+    //     x = 1;        am depasit deci return in default
+    //     xprec = 10; /* o valoare random*/
+    //     previousMillis2=millis();
+    //   }
+    //   else x = 5 ;/*RotireDreaptaRapid*/
+    // }
+
+
+    // /* daca a fost activat BB*/
+    // if(xprec == 3){
+    //   if(millis() - previousMillis3 > delay_BB)
+    //   {
+    //     x = 1;      /* am depasit deci return in default*/
+    //     xprec = 10; /* o valoare random*/
+    //     previousMillis3=millis();
+    //   }
+    //   else x = 5 ;/*RotireDreaptaRapid*/
+    // }
 
     /* && xprec==10 e ca sa verific daca nu sunt deja intr-o rotire*/
     if (digitalRead(BL)==0 && xprec==10)
