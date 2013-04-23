3386b1984400ec4b1216568c0f45d00e09b975f1
flaviusone <flaviusone@yahoo.com>
2013-04-23 14:23:19 +0300

    Corrected strategy pins

--- strategy.ino
+++ strategy.ino
@@ -1,7 +1,7 @@
 int strategy()
 {
-	int strat = 4 * digitalRead(strat_pin_0) + 2 * digitalRead(strat_pin_1) 
-	+ 1 * digitalRead(strat_pin_2);
+	int strat = 4 * digitalRead(strat_pin_2) + 2 * digitalRead(strat_pin_1) 
+	+ 1 * digitalRead(strat_pin_0);
 	return strat;
 }
 
